5883219458c3bd68a82390ff3209baeb292637d6
Sam King <sdking02@gmail.com>
2018-07-10 22:52:47 +0100

    Add control for toggling expanded views
    - toggle inactive players (default: hidden)
    - toggle expanded/simple view (default: expanded)

--- resources/views/players/leaderboard.blade.php
+++ resources/views/players/leaderboard.blade.php
@@ -8,7 +8,7 @@
 
 <table class="leaderboard">
 	<thead>
-		<tr>
+		<tr class="js-expanded-row">
 			<th colspan="11"></th>
 			<th colspan="3" class="handicap">Handicap</th>
 			<th colspan="3" class="handicap">Advantage</th>
@@ -27,22 +27,22 @@
 			<th>GD</th>
 			<th>Pts</th>
 			<th>Win %</th>
-			<th>Pld</th>
-			<th>W</th>
-			<th>L</th>
-			<th>Pld</th>
-			<th>W</th>
-			<th>L</th>
-			<th>Pts</th>
-			<th>F</th>
-			<th>A</th>
+			<th class="js-expanded">Pld</th>
+			<th class="js-expanded">W</th>
+			<th class="js-expanded">L</th>
+			<th class="js-expanded">Pld</th>
+			<th class="js-expanded">W</th>
+			<th class="js-expanded">L</th>
+			<th class="js-expanded">Pts</th>
+			<th class="js-expanded">F</th>
+			<th class="js-expanded">A</th>
 			<th>Last App</th>
 			<th>Form</th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach($players as $i => $player)
-		<tr>
+		<tr class="{{ $matches->first()->date->greaterThan($player->last_app) ? "js-inactive" : "" }}">
 			<td class="number">{{ $i+1 }}</td>
 			<td class="name">
 				<a href="{{ route('players.show', [$player->id] + Request::all()) }}">
@@ -58,15 +58,15 @@
 			<td>{{ $player->diff }}</td>
 			<td>{{ ($player->wins * 3) + $player->draws }}</td>
 			<td>{{ $player->win_percentage }}%</td>
-			<td>{{ $player->handicap_apps }}</td>
-			<td>{{ $player->handicap_wins }}</td>
-			<td>{{ $player->handicap_losses }}</td>
-			<td>{{ $player->advantage_apps }}</td>
-			<td>{{ $player->advantage_wins }}</td>
-			<td>{{ $player->advantage_losses }}</td>
-			<td>{{ round((($player->wins * 3) + $player->draws) / $player->played, 2) }}
-			<td>{{ round($player->gspg, 1) }}</td>
-			<td>{{ round($player->gcpg, 1) }}</td>
+			<td class="js-expanded">{{ $player->handicap_apps }}</td>
+			<td class="js-expanded">{{ $player->handicap_wins }}</td>
+			<td class="js-expanded">{{ $player->handicap_losses }}</td>
+			<td class="js-expanded">{{ $player->advantage_apps }}</td>
+			<td class="js-expanded">{{ $player->advantage_wins }}</td>
+			<td class="js-expanded">{{ $player->advantage_losses }}</td>
+			<td class="js-expanded">{{ round((($player->wins * 3) + $player->draws) / $player->played, 2) }}
+			<td class="js-expanded">{{ round($player->gspg, 1) }}</td>
+			<td class="js-expanded">{{ round($player->gcpg, 1) }}</td>
 			<td>
 				<a href="{{ route('matches.show', $player->last_app_id) }}">
 					{{ $player->last_app }}
@@ -77,5 +77,11 @@
 		@endforeach
 	</tbody>
 </table>
+
+<div class="leaderboard-controls">
+	<a onclick="document.querySelector('.leaderboard').classList.toggle('js-expanded-table')">Toggle expanded table</a>
+	|
+	<a onclick="document.querySelector('.leaderboard').classList.toggle('js-inactive-players-table')">Toggle active players</a>
+</div>
 
 @stop
