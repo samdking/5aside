035a9027dd4ac9b372c100e923e4a8171937c678
Sam King <sdking02@gmail.com>
2018-08-17 19:19:12 +0100

    Tidy up SeasonController

--- app/Queries/SeasonQuery.php
+++ app/Queries/SeasonQuery.php
@@ -7,35 +7,41 @@ use Illuminate\Http\Request;
 
 class SeasonQuery
 {
+	protected $players;
+	protected $matches;
+	protected $year;
+
 	public function __construct(Request $request, $year)
 	{
 		$request->year = $year;
-		$this->request = $request;
+		$this->year = $year;
+		$this->players = new PlayerQuery($request);
+		$this->matches = new MatchQuery($request);
 	}
 
 	public function get()
 	{
-		$players = (new PlayerQuery($this->request))->get();
+		$players = $this->players->get();
 
 		return [
-			'year' => $this->request->year,
+			'year' => $this->year,
 			'start_date' => $players->min('first_appearance'),
 			'end_date' => $this->seasonHasEnded() ? $players->max('last_appearance') : null,
 			'total_matches' => $this->matchCount(),
-			'matches' => (new MatchQuery($this->request))->get(),
+			'matches' => $this->matches->get(),
 			'leaderboard' => $players
 		];
 	}
 
 	protected function seasonHasEnded()
 	{
-		return $this->request->year < (new DateTime)->format('Y');
+		return $this->year < (new DateTime)->format('Y');
 	}
 
 	protected function matchCount()
 	{
 		return \DB::selectOne("SELECT COUNT(id) AS count FROM matches where YEAR(date) = ?", [
-			$this->request->year
+			$this->year
 		])->count;
 	}
 }
