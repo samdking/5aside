d4812db81d0126325da02362153ffe7a8aa4706b
Sam King <sdking02@gmail.com>
2020-09-14 19:39:22 +0100

    Add extra data to venue query

--- app/Queries/VenueQuery.php
+++ app/Queries/VenueQuery.php
@@ -19,7 +19,7 @@ class VenueQuery
 		$fields = collect($fields)->map(function($field) { return 'venues.' . $field; })->implode(', ');
 
 $query = <<<EOT
-		SELECT {$fields}, COUNT(matches.id) as match_count, MIN(date) AS first_match
+		SELECT {$fields}, COUNT(DISTINCT matches.id) as match_count, MIN(date) AS first_match
 		FROM venues
 		INNER JOIN matches ON matches.venue_id = venues.id
 		GROUP BY venues.id
@@ -29,7 +29,40 @@ EOT;
 		$matches = $this->matches->get()->groupBy('venue');
 
 		return collect(\DB::select($query))->each(function($venue) use ($matches) {
-			$venue->matches = $matches->get($venue->name);
+			$matches = $matches->get($venue->name);
+
+			$venue->total_goals = $matches->sum->total_goals;
+			$venue->total_attendance = $matches->sum->total_players;
+			$venue->average_attendance = round($matches->average->total_players);
+
+			$venue->highest_attendance = $matches->max(function($match) {
+				return $match->total_players;
+			});
+
+			$venue->highest_scoring_match = $matches->max(function($match) {
+				return $match->total_goals;
+			});
+
+			$venue->lowest_scoring_match = $matches
+				->filter(function($match) {
+					return ! is_null($match->team_a_scored);
+				})->min(function($match) {
+					return $match->total_goals;
+				});
+
+			$venue->highest_attendance_matches = $matches->filter(function($match) use ($venue) {
+				return $match->total_players == $venue->highest_attendance;
+			})->values();
+
+			$venue->highest_scoring_matches = $matches->filter(function($match) use ($venue) {
+				return $match->total_goals == $venue->highest_scoring_match;
+			})->values();
+
+			$venue->lowest_scoring_matches = $matches->filter(function($match) use ($venue) {
+				return $match->total_goals == $venue->lowest_scoring_match;
+			})->values();
+
+			$venue->matches = $matches;
 		});
 	}
 }
