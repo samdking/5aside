15bdeee01ee077f26d7b72ab309139f15aefc1ed
Sam King <sdking02@gmail.com>
2020-09-15 14:18:48 +0100

    Use new higher-order messages on collections

--- app/Queries/VenueQuery.php
+++ app/Queries/VenueQuery.php
@@ -32,24 +32,18 @@ EOT;
 			$matches = $matches->get($venue->name);
 
 			$venue->total_goals = $matches->sum->total_goals;
-			$venue->average_goals = round($matches->average->total_goals, 2);
+			$venue->average_goals = round($matches->filter(function($match) {
+				return ! is_null($match->team_a_scored);
+			})->average->total_goals, 2);
 			$venue->total_attendance = $matches->sum->total_players;
 			$venue->average_attendance = round($matches->average->total_players, 2);
-
-			$venue->highest_attendance = $matches->max(function($match) {
-				return $match->total_players;
-			});
-
-			$venue->highest_scoring_match = $matches->max(function($match) {
-				return $match->total_goals;
-			});
+			$venue->highest_attendance = $matches->max->total_players;
+			$venue->highest_scoring_match = $matches->max->total_goals;
 
 			$venue->lowest_scoring_match = $matches
 				->filter(function($match) {
 					return ! is_null($match->team_a_scored);
-				})->min(function($match) {
-					return $match->total_goals;
-				});
+				})->min->total_goals;
 
 			$venue->highest_attendance_matches = $matches->filter(function($match) use ($venue) {
 				return $match->total_players == $venue->highest_attendance;
