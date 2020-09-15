507c4231becfc859e827b0e9b242bdb5809f9b17
Sam King <sdking02@gmail.com>
2020-09-15 14:16:36 +0100

    Upgrade to Laravel 5.6

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends HttpKernel {
 	 * @var array
 	 */
 	protected $middleware = [
-		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+		\App\Http\Middleware\CheckForMaintenanceMode::class,
 	];
 
     /**
