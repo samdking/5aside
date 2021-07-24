61b551b2c16507d3a7a31a8bb78d887af9e4f746
Sam King <sdking02@gmail.com>
2021-07-24 21:57:45 +0100

    Reformat query

--- app/Queries/PlayedWithAgainst.php
+++ app/Queries/PlayedWithAgainst.php
@@ -14,18 +14,18 @@ class PlayedWithAgainst
     public function get()
     {
 		$query = <<<SQL
-        select players.*, SUM(teammates.id = teams.id) AS `with`, SUM(teammates.id != teams.id) AS `against`
-    from players
-    join player_team pt on pt.player_id = players.id
-    join teams on teams.id = pt.team_id
-    join matches on matches.id = teams.match_id
-    left join (
-        select t.*
-        from teams t
-        join player_team pt on pt.team_id = t.id AND pt.player_id = ?
+        SELECT players.*, SUM(teammates.id = teams.id) AS `with`, SUM(teammates.id != teams.id) AS `against`
+    FROM players
+    INNER JOIN player_team pt ON pt.player_id = players.id
+    INNER JOIN teams ON teams.id = pt.team_id
+    INNER JOIN matches ON matches.id = teams.match_id
+    INNER JOIN (
+        SELECT t.*
+        FROM teams t
+        INNER JOIN player_team pt ON pt.team_id = t.id AND pt.player_id = ?
     ) teammates ON teammates.match_id = teams.match_id
     WHERE players.id != ? AND date >= ? AND date <= ?
-    group by players.id
+    GROUP BY players.id
 SQL;
 
         $placeholders = [
