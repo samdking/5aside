e72a2ea6bcb559cc7dcc3283ffeff2ef664ad35c
Sam King <sdking02@gmail.com>
2021-08-03 22:47:45 +0100

    Rework to include results

--- app/Queries/FormQuery.php
+++ app/Queries/FormQuery.php
@@ -18,13 +18,13 @@ class FormQuery
 	public function get($forYear = null)
 	{
 		if (is_null($this->query) && is_null($forYear)) {
-			$this->query = $this->query($this->limit());
+			$this->query = $this->query();
 		}
 
-		if (is_null($forYear)) return $this->query;
+		if (is_null($forYear)) return $this->query->take($this->limit());
 
 		if (is_null($this->groupByYear)) {
-			$this->groupByYear = $this->query()->groupBy('year');
+			$this->groupByYear = $this->query->groupBy('year');
 		}
 
 		if ( ! $this->groupByYear->has($forYear)) {
@@ -34,32 +34,29 @@ class FormQuery
 		return $this->groupByYear[$forYear];
 	}
 
-	protected function query($limit = null)
+	protected function query()
 	{
 		$placeholders = [
 			(new Filters\FromDate)->get($this->request),
 			(new Filters\ToDate)->get($this->request),
 		];
 
-		$limit = $limit ? "LIMIT {$limit}" : '';
-
 		$query = <<<SQL
-		select matches.date, group_concat(player_id) AS players
+		select matches.date, IF(is_void, 'Void', IF(winners, 'Win', IF(draw, 'Draw', 'Loss'))) as result, player_id
 		from matches
 		join teams on teams.match_id = matches.id
 		join player_team pt on pt.team_id = teams.id
 		WHERE date >= ? AND date <= ?
-		group by matches.id
+		group by matches.date, team_id, player_id
 		order by date desc
-		{$limit}
 SQL;
 
-		return collect(\DB::select($query, $placeholders))->map(function($match) {
+		return collect(\DB::select($query, $placeholders))->groupBy('date')->map(function($players, $date) {
 			return (object)[
-				'players' => collect(explode(',', $match->players)),
-				'year' => substr($match->date, 0, 4),
+				'players' => $players->keyBy('player_id')->map->result,
+				'year' => substr($date, 0, 4),
 			];
-		});
+		})->values();
 	}
 
 	protected function limit()
