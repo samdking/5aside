1446578fa8b6c7fc2961dfe9f209363d2404d015
Sam King <sdking02@gmail.com>
2021-08-07 13:18:33 +0100

    Remove seasons/all endpoint for v1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,7 +29,6 @@ Route::group(['prefix' => 'api'], function() {
 	Route::get('players', 'DataController@players');
 	Route::get('players/{player}', 'DataController@player');
 	Route::get('seasons', 'DataController@seasons');
-	Route::get('seasons/all', 'DataController@allSeasons');
 	Route::get('seasons/{season}', 'DataController@seasons');
 	Route::group(['prefix' => 'v2'], function() {
 		Route::get('seasons', 'DataController@allSeasons');
