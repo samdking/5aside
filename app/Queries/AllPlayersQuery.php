51f9a4f2de5beb7a2d15baaeb6152d4b142bea57
Sam King <sdking02@gmail.com>
2022-10-28 21:35:44 +0100

    Remove unused property

--- app/Queries/AllPlayersQuery.php
+++ app/Queries/AllPlayersQuery.php
@@ -6,12 +6,10 @@ use Illuminate\Http\Request;
 
 class AllPlayersQuery
 {
-	protected $request;
 	protected $streaks;
 
 	public function __construct(Request $request)
 	{
-        $this->request = $request;
         $this->players = new PlayerQuery($request);
 		$this->streaks = new PlayerStreakQuery($request);
 	}
